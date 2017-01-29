e4f16093f38f0aec07f74c4d1285886a084d2b8a
Jade Chunnananda <jade.jch@gmail.com>
2017-01-29 01:27:17 -0800

    Darksky API

--- js/config.example.js
+++ js/config.example.js
@@ -2,24 +2,24 @@
 
 // Obtain from dev.twitter.com
 var config = {
-  consumer_key:         'tg4zxQa7FqxcrrksmObVqnjfQ',
-  consumer_secret:      'IyLyUgZFbt6Hi0bwRQObgEC1H1CHGVrz9AcYdngDwtY52eYEKm',
-  access_token:         '1923070242-MDEgjNbTxkysqe91s1b0xQGwp2nw1mhjiJxNnBQ',
-  access_token_secret:  'eUBzL1c148WAYatmlN6CwiHk6i3KGmOr5PyybEizDS0Ie',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           5*1000,  // 5s interval - Maximum 180 calls per minute
 };
 
 // Twitter user Parameters, from API documentation
 var params = {
-  slug: 'Sneakers',
-  owner_screen_name: 'GnarlyNarwhal1',
-  count: 5,
+  slug: '',
+  owner_screen_name: '',
+  count: ,
 }
 // Weather API config - Darksky API
-var weatherAPI = "https://api.darksky.net/forecast/2519fab70efad46ca9f278085daf427a/37.8267,-122.4233";
+var weatherAPI = ;
 
 // NYTimes API config
-var nyTimes = "http://api.nytimes.com/svc/topstories/v1/home.json?api-key=cc5ee843469c4fbf95ce3aca5ee099f9";
+var nyTimes = ;
 
 
 // So that variable can be accessed
